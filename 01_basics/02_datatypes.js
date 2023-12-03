ac4247456c54c3c3a320b0be50b7328a6932a5c6
HimanshuLanjewar28 <115108177+HimanshuLanjewar28@users.noreply.github.com>
2023-12-03 09:09:15 +0000

    Data Types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";
+//alert(3+3)
+
+let name = "HImanshu" 
+let age = 21
+let isLoggedIn = false 
+
+// number => 2 to the power 53
+// bigint=> 
+// string => " "
+// boolean => true/false
+// null => standalone value 
+// undefined
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); //=> undefined
+console.log(typeof null); // => object
