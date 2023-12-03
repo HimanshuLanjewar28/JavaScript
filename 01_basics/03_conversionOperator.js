69f7032960a7ce5afc1eb38f853dd3868fb2e020
HimanshuLanjewar28 <115108177+HimanshuLanjewar28@users.noreply.github.com>
2023-12-03 12:53:54 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,9 @@
+let score = "33"
+console.log(typeof score);
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
