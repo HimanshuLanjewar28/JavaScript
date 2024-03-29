ba2c52048f05418e305760c55e6a5dc45636f10d
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-03-29 22:13:05 +0530

    javascript

--- Basics_JS_03/iife.js
+++ Basics_JS_03/iife.js
@@ -1,2 +1,13 @@
 // Immediately Invoked Function Expressions (IIFE)
 
+
+// 1)Named IIFE
+(function one() {
+    console.log(`DB CONNECTED`);
+})();
+
+
+// 2)UnNamed IIFE
+( (name1)  => {
+    console.log(`DB CONNECTED TWO ${name1}`);
+} )("Himanshu");
