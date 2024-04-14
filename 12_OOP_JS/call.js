26b3f058f6d4a56e68ac00b172707e0301abcd8d
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-04-14 16:43:42 +0530

    OOPs and other advace JS

---
+++ 12_OOP_JS/call.js
@@ -0,0 +1,17 @@
+// call and this
+
+function setUsername(username){
+    //compkex db call
+    this.username = username
+}
+
+function createUser(username, email, password){
+    setUsername.call(this,username)
+
+    this.email = email
+    this.password = password
+
+}
+
+const chai = new createUser("chai", "chai@gmail.com", "123")
+console.log(chai);
